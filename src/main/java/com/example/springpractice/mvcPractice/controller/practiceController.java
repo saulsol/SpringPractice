23891f24f599435fd897a86232c600eafcfbca26
saul <dlathf3210@naver.com>
2023-08-29 17:51:16 +0900

    0829

--- src/main/java/com/example/springpractice/mvcPractice/controller/practiceController.java
+++ src/main/java/com/example/springpractice/mvcPractice/controller/practiceController.java
@@ -1,15 +1,33 @@
 package com.example.springpractice.mvcPractice.controller;
 
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 @RestController
 public class practiceController {
 
     @GetMapping("/redirect")
-    public String redirect(){
-        return "redirect:/";
+    public ResponseEntity<?> redirect(){
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(URI.create("/"));
+
+        return new ResponseEntity<>(headers, HttpStatus.MOVED_PERMANENTLY);
+        // MOVED_PERMANENTLY 는 영구적으로 해당 주소는 사용하지 않으니
+        // 서버에서 알려준 주소로 다시 요청을 보내라는 뜻이다.
     }
+
+    @GetMapping("/")
+    public String basicPage(){
+        return "basic";
+    }
+
+
 
 }
