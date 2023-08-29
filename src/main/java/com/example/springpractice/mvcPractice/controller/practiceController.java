d8a0a2a923b255b346a6f172a8205d7cb1f4b297
saul <dlathf3210@naver.com>
2023-08-30 08:55:25 +0900

    0830

--- src/main/java/com/example/springpractice/mvcPractice/controller/practiceController.java
+++ src/main/java/com/example/springpractice/mvcPractice/controller/practiceController.java
@@ -4,30 +4,36 @@ package com.example.springpractice.mvcPractice.controller;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
 
-@RestController
+@Controller
 public class practiceController {
 
     @GetMapping("/redirect")
-    public ResponseEntity<?> redirect(){
-
+    public ResponseEntity<?> redirect() {
         HttpHeaders headers = new HttpHeaders();
         headers.setLocation(URI.create("/"));
-
-        return new ResponseEntity<>(headers, HttpStatus.MOVED_PERMANENTLY);
-        // MOVED_PERMANENTLY 는 영구적으로 해당 주소는 사용하지 않으니
-        // 서버에서 알려준 주소로 다시 요청을 보내라는 뜻이다.
+        return ResponseEntity.status(HttpStatus.MOVED_PERMANENTLY) // 301 Moved Permanently
+                .headers(headers)
+                .build();
     }
+
+
+
+    @GetMapping("/redirect/prefix")
+    public String redirectPrefix() {
+        return "redirect:/";
+    }
+
 
     @GetMapping("/")
     public String basicController(){
-        return "리다이렉트된 값입니다.";
+        return "index";
     }
-
 
 
 }
